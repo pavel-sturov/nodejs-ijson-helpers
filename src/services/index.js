cda041b2efe4736dff4454b960ba839d5ea1e193
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2020-06-19 13:22:23 +0300

    Fix export S3FileUpload

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const BaseConnection = require('./baseDb');
 const Connection     = require('./db');
 const JsonQuery      = require('./query');
+const S3FileUpload   = require('./S3FileUpload');
 
 module.exports = {
 	BaseConnection,
 	Connection,
 	JsonQuery,
+	S3FileUpload,
 };
